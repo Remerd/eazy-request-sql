5e747f4ccac478d2243ee04ee74b89f63916e997
Remerd <32716064+Remerd@users.noreply.github.com>
2020-08-18 18:55:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ con = pymysql.connect('webvrdev.pro.izobretarium.ru', 'energo','zPMoL8HB', 'ener
 
 cur = con.cursor()
 cur.execute("SELECT * FROM objects")
+rows = cur.fetchall()
 
 for row in rows:
         print(str(row[0])+"\t"+str(row[1])+"\t"+str(row[2])+"\t"+str(row[3])+"\t"+str(row[4])+"\t"+str(row[5])+"\t"+str(row[6])+"\t"+str(row[7])+"\t"+str(row[8]))
